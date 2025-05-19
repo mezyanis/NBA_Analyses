42e0a10af7ed880484a4f48f042d023d47c28884
mezyanis <yanismeziani18@gmail.com>
2025-05-19 14:29:29 +0200

    changement de source de données.

---
+++ get_data_from_basketball_reference.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_data_from_basketball_reference(year_start, year_end):
+
+    list_data = []
+    for year in range(year_start, year_end+1):
+        url = f'https://www.basketball-reference.com/leagues/NBA_{year}_advanced.html'
+        html = urlopen(url)
+        soup = BeautifulSoup(html)
+        headers = [th.getText() for th in soup.find_all('tr')[0].find_all('th')]
+        headers = headers[1:] + ['Season'] # columns
+        
+        rows = soup.find_all('tr')
+        player_stats_1 = [[td.getText() for td in rows[i].find_all('td')] for i in range(len(rows))]  # rows
+
+        for row in player_stats_1:
+            if row:  
+                row.append(str(year))
+
+        tmp_df = pd.DataFrame(player_stats_1, columns=headers)
+        tmp_mat = tmp_df.values
+        list_data.append(tmp_mat)
+
+    return list_data, headers
+
+
+def build_df(data,  columns):
+    df = pd.DataFrame(data[0][1:], columns=columns)
+    for i in range(1, len(data)):
+        tmp_df = pd.DataFrame(data[i], columns=columns)
+        df = pd.concat([df, tmp_df], ignore_index=True)
+    return df
+
+
+def create_csv(path, df: pd.DataFrame):
+    df.to_csv(path, index=False)
+    
+    
+
+data, columns = get_data_from_basketball_reference(2015, 2024)
+df = build_df(data, columns)
+create_csv('advanced_data_2015_2024.csv', df)
