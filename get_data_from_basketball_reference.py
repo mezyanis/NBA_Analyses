4c5f55f5e3d826247d9b3c4dbfcafc5986371fac
mezyanis <yanismeziani18@gmail.com>
2025-05-21 11:43:53 +0200

    Structuration du notebook et Analyse exploratoire

--- get_data_from_basketball_reference.py
+++ get_data_from_basketball_reference.py
@@ -28,6 +28,30 @@ def get_data_from_basketball_reference(year_start, year_end):
     return list_data, headers
 
 
+def get_data_from_nba(years):
+
+    list_data = []
+    for year in years:
+        url = f'https://www.nba.com/stats/players/advanced?SeasonType=Regular+Season&Season={year}'
+        html = urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        trs = soup.find_all('table')
+        print(trs)
+"""
+        headers = [th.getText() for th in soup.find_all('tr', class_='com_headers__mzI_m')[0].find_all('th')]
+        print(headers)
+         # columns
+        
+        rows = soup.find('tbody', class_='Crom_body__UYOcU').find_all('tr')
+        player_stats_1 = [[td.getText() for td in rows[i].find_all('td')] for i in range(len(rows))]  # rows
+
+        tmp_df = pd.DataFrame(player_stats_1, columns=headers)
+        tmp_mat = tmp_df.values
+        list_data.append(tmp_mat)
+
+    return list_data, headers
+"""
+
 def build_df(data,  columns):
     df = pd.DataFrame(data[0][1:], columns=columns)
     for i in range(1, len(data)):
@@ -41,6 +65,8 @@ def create_csv(path, df: pd.DataFrame):
     
     
 
-data, columns = get_data_from_basketball_reference(2015, 2024)
-df = build_df(data, columns)
-create_csv('advanced_data_2015_2024.csv', df)
+#data, columns = get_data_from_basketball_reference(2015, 2024)
+
+data, columns = get_data_from_nba(['2023-24'])
+#df = build_df(data, columns)
+#create_csv('advanced_data_2023_24.csv', df)
